510c899e30ff0e21c28e8bbf0cea88c515fd1cc6
boiniraj <161281335+boiniraj@users.noreply.github.com>
2026-04-04 16:33:01 +0530

    add user

---
+++ User_Details/src/main/java/com/nt/user/UserDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.nt.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserDetailsApplication.class, args);
+	}
+
+}
---
+++ User_Details/src/main/java/com/nt/user/dto/UserDetails.java
@@ -0,0 +1,12 @@
+package com.nt.user.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDetails {
+	
+	private String userName;
+	private String phno;
+	private String addr;
+
+}
